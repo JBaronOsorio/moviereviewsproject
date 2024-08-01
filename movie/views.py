5db19e0fd3dae689699ff81fb925dd607e96c37a
JBaronOsorio <jjbarono@eafit.edu.co>
2024-07-31 22:52:26 -0500

    cambios en la funcion home

--- movie/views.py
+++ movie/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def home(request):
     #return HttpResponse('<h1>Welcome to home page</h1>')
-    return render(request, 'home.html', {'name':'Juan Jose Baron'})
+    return render(request, 'home.html', {'name':'Juan Jose Baron Osorio'})
 
 def about(request):
     return HttpResponse('<h1>Welcome to about page</h1>')
